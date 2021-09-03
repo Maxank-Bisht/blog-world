833561d23bf9659beef8ddc0a3021a1c86d9c324
Mayank Bisht <bishtmayank799@gmail.com>
2021-09-03 15:51:46 +0530

    integrated auth

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoute');
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 
 const username = process.env.DATABASE_USERNAME;
 const password = process.env.DATABASE_PASSWORD;
@@ -11,42 +15,38 @@ const dbname = process.env.DATABASE_NAME;
 
 const port = process.env.PORT || 3000;
 
-const blogRoutes = require('./routes/blogRoute');
-
 //express app
 const app = express();
+
+//middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+//register view engine
+app.set('view engine', 'ejs');
 
 //connect to MongoDB
 const dbURI = `mongodb+srv://${username}:${password}@cluster0.qfxbp.mongodb.net/${dbname}`;
 
 mongoose
-	.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+	.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 	.then((result) => {
 		console.log('connected to DB');
 		//listen for request
 		app.listen(port);
 	})
-	.catch((err) => {
-		console.log(err);
-	});
+	.catch((err) => console.log(err));
 
-//register view engine
-app.set('view engine', 'ejs');
-
-//middleware & static files
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-	res.redirect('/blogs');
-});
-
-app.get('/about', (req, res) => {
-	res.render('about', { title: 'About' });
-});
+//routes
+app.get('*', checkUser);
+app.get('/', (req, res) => res.redirect('/blogs'));
+app.get('/about', (req, res) => res.render('about', { title: 'About' }));
 
 //blogs
 app.use('/blogs', blogRoutes);
+app.use(authRoutes);
 
 //404 page
 app.use((req, res) => {
---
+++ controllers/authController.js
@@ -0,0 +1,79 @@
+require('dotenv').config();
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+const jwt_secret = process.env.JWT_SECRET;
+
+//error handler
+const handleErrors = (err) => {
+	let errors = { email: '', password: '' };
+
+	//incorrect email
+	if (err.message === 'Incorrect Email') {
+		errors.email = 'This email is not register.';
+	}
+	//incorrect email
+	if (err.message === 'Incorrect Password') {
+		errors.password = 'The password is incorrect';
+	}
+
+	if (err.code === 11000) {
+		//duplicate email
+		errors.email = 'This email is already registered';
+		return errors;
+	}
+
+	//validation errors
+	if (err.message.includes('user validation failed')) {
+		Object.values(err.errors).forEach(({ properties }) => {
+			errors[properties.path] = properties.message;
+		});
+	}
+	return errors;
+};
+
+const maxAge = 3 * 24 * 60 * 60;
+//creating web tokens
+const createToken = (id) => {
+	return jwt.sign({ id }, jwt_secret, {
+		expiresIn: maxAge,
+	});
+};
+
+module.exports.signup_get = (req, res) => {
+	res.render('signup', { title: 'Sign Up' });
+};
+module.exports.login_get = (req, res) => {
+	res.render('login', { title: 'Login' });
+};
+module.exports.signup_post = async (req, res) => {
+	const { email, password } = req.body;
+	try {
+		const user = await User.create({ email, password });
+		console.log(user);
+		const token = createToken(user._id);
+		res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+		res.status(201).json({ user: user._id });
+	} catch (error) {
+		const errors = handleErrors(error);
+		res.status(400).json({ errors });
+	}
+};
+module.exports.login_post = async (req, res) => {
+	const { email, password } = req.body;
+
+	try {
+		const user = await User.login(email, password);
+		const token = createToken(user._id);
+		res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+		res.status(200).json({ user: user._id });
+	} catch (err) {
+		const errors = handleErrors(err);
+		res.status(400).json({ errors });
+	}
+};
+
+module.exports.logout_get = (req, res) => {
+	res.cookie('jwt', '', { maxAge: 1 });
+	res.redirect('/');
+};
